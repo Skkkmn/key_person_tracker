2432554ebbd88b25ffea326a4c6c779ca8676af3
Skkkmn <2045244661@qq.com>
2026-06-18 18:44:27 +0800

    add mobile-app/gradlew.bat

---
+++ mobile-app/gradlew.bat
@@ -0,0 +1,15 @@
+@rem Gradle wrapper for Windows
+@if "%DEBUG%"=="" @echo off
+setlocal enabledelayedexpansion
+
+set DIRNAME=%~dp0
+if "%DIRNAME%"=="" set DIRNAME=.
+set APP_BASE_NAME=%~n0
+set APP_HOME=%DIRNAME%
+
+set DEFAULT_JVM_OPTS="-Xmx64m" "-Xms64m"
+set GRADLE_OPTS=%GRADLE_OPTS% -Dorg.gradle.appname=%APP_BASE_NAME%
+
+set CLASSPATH=%APP_HOME%gradle\wrapper\gradle-wrapper.jar
+
+"%JAVA_EXE%" %DEFAULT_JVM_OPTS% %JAVA_OPTS% %GRADLE_OPTS% -classpath "%CLASSPATH%" org.gradle.wrapper.GradleWrapperMain %*
